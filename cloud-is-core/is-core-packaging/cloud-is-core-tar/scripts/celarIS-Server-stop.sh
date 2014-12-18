51b0b6d9eb0a1abe3b6508e293d8f935e23b49c8
Thanasis Foudoulis <foudoulisathanasios@gmail.com>
2014-12-18 19:00:50 +0200

    Minor fixes

--- cloud-is-core/is-core-packaging/cloud-is-core-tar/scripts/celarIS-Server-stop.sh
+++ cloud-is-core/is-core-packaging/cloud-is-core-tar/scripts/celarIS-Server-stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Grabs and kill a process from the pidlist that has the word CelarIS-Service
+# Grabs and kill a process from the pidlist that has the word celarIS-Service
 
-pid=`ps aux | grep CelarIS-Service | awk '{print $2}'`
+pid=`ps aux | grep celarIS-Service | awk '{print $2}'`
 kill -9 $pid > /dev/null 2>&1 &
